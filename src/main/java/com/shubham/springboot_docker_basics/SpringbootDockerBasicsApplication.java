44e1f06b6380b02fcdccbfcba8b6729ec309240b
Shubham Aggarwal <sbmaggarwal@gmail.com>
2025-07-15 14:03:55 +0530

    first commit

---
+++ src/main/java/com/shubham/springboot_docker_basics/SpringbootDockerBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.springboot_docker_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/springboot_docker_basics/SpringbootDockerBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.springboot_docker_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
